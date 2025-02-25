dac3d02a4aa168b40ad370e16a1a236f821abdce
jesus <jmoral.jj@gmail.com>
2025-02-25 13:07:39 +0100

    Deploy

--- server.js
+++ server.js
@@ -5,10 +5,11 @@ const app = express();
 const apiRouter = jsonServer.router("db.json"); 
 const middlewares = jsonServer.defaults();
 app.use("/api", middlewares, apiRouter); 
-app.use(express.static(path.join(__dirname, "dist/practica-final")));
+app.use(express.static(path.join(__dirname, "dist/practica-final/browser")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "dist/practica-final/index.html"));
+  res.sendFile(path.join(__dirname, "dist/practica-final/browser/index.html"));
 });
+app.use("/api", apiRouter);
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
